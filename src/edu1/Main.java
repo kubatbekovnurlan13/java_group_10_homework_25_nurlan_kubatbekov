b40c3e447a1b8b5ef93b63e009c732d5520e5daf
Nurlan_Kubatbekov <kubatbekovnurlan13@gmail.com>
2021-12-12 00:43:48 +0600

    #7 Added complicated version of game

--- src/edu1/Main.java
+++ src/edu1/Main.java
@@ -10,8 +10,21 @@ public class Main {
         allRounds();
     }
 
-    public static String makeOption(int index) {
-        Options[] arr = new Options[]{Options.PAPER, Options.SCISSORS, Options.ROCK};
+    public static String makeNormalOption(int index) {
+        Options[] arr = new Options[]{
+                Options.PAPER,
+                Options.SCISSORS,
+                Options.ROCK};
+        return arr[index] + "";
+    }
+
+    public static String makeComlicatedOption(int index) {
+        OptionsComplicated[] arr = new OptionsComplicated[]{
+                OptionsComplicated.PAPER,
+                OptionsComplicated.SCISSORS,
+                OptionsComplicated.ROCK,
+                OptionsComplicated.LIZARD,
+                OptionsComplicated.SPOCK};
         return arr[index] + "";
     }
 
@@ -22,7 +35,7 @@ public class Main {
         int z = 1;
         while (z < 7) {
             System.out.println("----- " + z + " round -----");
-            int[][] res = round(comp, user);
+            int[][] res = complicatedRound(comp, user);
             comp = res[0];
             user = res[1];
             z++;
@@ -47,20 +60,20 @@ public class Main {
 
     }
 
-    public static int[][] round(int[] comp, int[] user) {
+    public static int[][] normalRound(int[] comp, int[] user) {
         Random rnd = new Random();
         int randomNumber = rnd.nextInt(3) + 1;
 
-        printConditions();
+        printNormalConditions();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter comand: ");
         String input = sc.nextLine();
         try {
             int intInput = tryInt(input);
-            checkComand(intInput);
+            normalCheckComand(intInput);
             int userInput = intInput - 1;
 
-            String userOption = makeOption(userInput);
+            String userOption = makeNormalOption(userInput);
 
             switch (userOption) {
                 case "PAPER":
@@ -102,15 +115,137 @@ public class Main {
             }
         } catch (ParseException | IllegalArgumentException ex) {
             System.out.println("WARNING!!! =====> Please enter right comand!");
-            round(comp, user);
+            normalRound(comp, user);
         }
         return new int[][]{comp, user};
     }
 
-    public static void printConditions() {
-        System.out.println("PAPER     => enter 1");
-        System.out.println("SCISSORS  => enter 2");
-        System.out.println("ROCK      => enter 3");
+    public static int[][] complicatedRound(int[] comp, int[] user) {
+        Random rnd = new Random();
+        int randomNumber = rnd.nextInt(5) + 1;
+
+        printComplicatedConditions();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter comand: ");
+        String input = sc.nextLine();
+        try {
+            int intInput = tryInt(input);
+            complicatedCheckComand(intInput);
+            int userInput = intInput - 1;
+
+            String userOption = makeComlicatedOption(userInput);
+
+            switch (userOption) {
+                case "PAPER":
+                    if (randomNumber == 1) {
+                        comp[2] = comp[2] + 1;
+                        user[2] = user[2] + 1;
+                    } else if (randomNumber == 2) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else if (randomNumber == 3) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else if (randomNumber == 4) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    }
+                    break;
+                case "SCISSORS":
+                    if (randomNumber == 1) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else if (randomNumber == 2) {
+                        comp[2] = comp[2] + 1;
+                        user[2] = user[2] + 1;
+                    } else if (randomNumber == 3) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else if (randomNumber == 4) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    }
+                    break;
+                case "ROCK":
+                    if (randomNumber == 1) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else if (randomNumber == 2) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else if (randomNumber == 3) {
+                        comp[2] = comp[2] + 1;
+                        user[2] = user[2] + 1;
+                    } else if (randomNumber == 4) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    }
+                    break;
+                case "LIZARD":
+                    if (randomNumber == 1) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else if (randomNumber == 2) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else if (randomNumber == 3) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else if (randomNumber == 4) {
+                        comp[2] = comp[2] + 1;
+                        user[2] = user[2] + 1;
+                    } else {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    }
+                    break;
+                case "SPOCK":
+                    if (randomNumber == 1) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else if (randomNumber == 2) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else if (randomNumber == 3) {
+                        user[0] = user[0] + 1;
+                        comp[1] = comp[1] + 1;
+                    } else if (randomNumber == 4) {
+                        comp[0] = comp[0] + 1;
+                        user[1] = user[1] + 1;
+                    } else {
+                        comp[2] = comp[2] + 1;
+                        user[2] = user[2] + 1;
+                    }
+                    break;
+            }
+        } catch (ParseException | IllegalArgumentException ex) {
+            System.out.println("WARNING!!! =====> Please enter right comand!");
+            complicatedRound(comp, user);
+        }
+        return new int[][]{comp, user};
+    }
+
+    public static void printNormalConditions() {
+        System.out.println("● PAPER     => enter 1");
+        System.out.println("● SCISSORS  => enter 2");
+        System.out.println("● ROCK      => enter 3");
+    }
+
+    public static void printComplicatedConditions() {
+        System.out.println("● PAPER     => enter 1");
+        System.out.println("● SCISSORS  => enter 2");
+        System.out.println("● ROCK      => enter 3");
+        System.out.println("● LIZARD    => enter 4");
+        System.out.println("● SPOCK     => enter 5");
     }
 
     public static int tryInt(String birth) throws ParseException {
@@ -118,8 +253,14 @@ public class Main {
         return nf.parse(birth).intValue();
     }
 
-    public static void checkComand(int intInput) {
+    public static void normalCheckComand(int intInput) {
         if (intInput <= 0 || intInput > 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void complicatedCheckComand(int intInput) {
+        if (intInput <= 0 || intInput > 5) {
             throw new IllegalArgumentException();
         }
     }
---
+++ src/edu1/OptionsComplicated.java
@@ -0,0 +1,5 @@
+package edu1;
+
+public enum OptionsComplicated {
+    PAPER, SCISSORS, ROCK, LIZARD, SPOCK
+}
